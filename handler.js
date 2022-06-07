b1c9909e776de2bc1b10fdd31875c630b6a78a32
Hafidz Abdillah <84985914+Sad404-Cyber@users.noreply.github.com>
2022-06-07 13:40:49 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -2381,6 +2381,7 @@ break
                 kagura.sendMessage(m.chat, { audio: { url: anu.result.mp4aLink }, mimetype: 'audio/mpeg', fileName: anu.result.lagu+'.m4a' }, { quoted: msg })
             }
             break
+           
             case 'soundcloud': case 'scdl': {
                 if (!text) throw 'No Query Title'
                 m.reply(mess.wait)
