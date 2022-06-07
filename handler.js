b8a857e2d021d37bf35f987da99126c9f0decd99
Hafidz Abdillah <84985914+Sad404-Cyber@users.noreply.github.com>
2022-06-07 22:43:22 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -1864,7 +1864,7 @@ break
 	    case 'ytmp3': case 'ytaudio': {
                 if (!text) throw `Example : ${prefix + command} https://youtube.com/watch?v=PtFMh6Tccag%27`
             axios
-                .get(api('zenz', '/api/ytaudio2',  { url: text }, 'apikey'))
+                .get(`https://api.lolhuman.xyz/api/ytaudio2?apikey=ThadzBotZ&url=${text}`)
                 .then(({ data }) => {
                     var caption = `❖ Title    : *${data.result.title}*\n`
                     caption += `❖ Size     : *${data.result.size}*`
