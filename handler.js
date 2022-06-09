5165a3c26b0e01c3e2aede75b2aecd58e5074624
Hafidz Abdillah <84985914+Sad404-Cyber@users.noreply.github.com>
2022-06-09 09:00:34 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -1652,7 +1652,7 @@ break
 	    break
 	       case 'attp': case 'ttp': case 'attp2': case 'ttp2': case 'ttp3': case 'ttp4': {
            if (!text) throw `Example : ${prefix + command} text`
-           await kagura.sendMedia(m.chat, `https://api.lolhuman.xyz/api/${command}?apikey=ThadzBotZ&text=${text}`, 'kagura', 'morou', m, {asSticker: true}
+           await kagura.sendMedia(m.chat, `https://api.lolhuman.xyz/api/${command}?apikey=ThadzBotZ&text=${text}`, 'kagura', 'morou', m, {asSticker: true})
          }
          break
 	       case 'smeme': case 'stickmeme': case 'stikmeme': case 'stickermeme': case 'stikermeme': {
