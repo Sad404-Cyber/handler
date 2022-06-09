2a008e43c98a52426ab81520edec120ff0c014eb
Hafidz Abdillah <84985914+Sad404-Cyber@users.noreply.github.com>
2022-06-09 22:42:03 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -3315,6 +3315,18 @@ Jika Ada Fitur Error Atau Bug Segera Lapor Ke Owner Bot
 • #stalktwitter [query]
 • #stalkgithub [query]
 
+ ❏ *Other Menu*
+• #ssweb [query]
+• #ssweb2 [query]
+• #shortlink [query]
+• #gimage [query]
+• #pinterest [query]
+• #wallpaper [query]
+• #wikimedia [query]
+• #ytsearch [query]
+• #ringtone [query]
+• #stalk [option] [query]
+
  ❏ *Random Menu*
 • #art
 • #bts
